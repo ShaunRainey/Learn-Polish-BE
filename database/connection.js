c8e1470635c7762ed5d36bf7a462ddd0568593c9
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2025-03-29 12:11:24 +0000

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,22 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../ini.env") });
 
+// const dotenvResult = require("dotenv").config();
+// if (dotenvResult.error) {
+//   throw new Error("Failed to load .env file");
+// }
+
+// console.log("Environment Variables Loaded:", process.env);
+
+
+
 const { Pool } = require("pg");
+
+if (process.env.error) {
+  console.error("Error loading .env file:", process.env.error);
+} else {
+  console.log("Environment variables loaded successfully:", process.env);
+}
 
 // if (!process.env.PGDATABASE) {
     //     throw new Error("No PGDatabase configured")
@@ -15,7 +30,7 @@ const { Pool } = require("pg");
         port: process.env.PGPORT,
     });
     
-    console.log(db);
+    // console.log(db);
     
     // console.log(process.env.PGDATABASE)
 module.exports = db;

d4b93cea933c4b492008662ee38c41a842fe6382
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2025-05-09 20:32:56 +0100

    swap prep by case to a query

--- Controllers/prepositionControllers.js
+++ Controllers/prepositionControllers.js
@@ -1,26 +1,23 @@
-const { selectPrepositions, selectPrepositionsByCase } = require("../Models/prepositionModels")
+const { selectPrepositions } = require("../Models/prepositionModels")
 const { capitaliseFirst } = require("../database/utils")
 
-function getPrepositions(req, res) {
-    selectPrepositions().then((data) => {
+function getPrepositions(req, res, next) {
+    const { grammaticalCase } = req.query
+    const capitalisedCase = capitaliseFirst(grammaticalCase);
+    const acceptedValues = ["Genitive", "Instrumental", "Locative", "Accusative"];
+
+    if (grammaticalCase && !acceptedValues.includes(capitalisedCase)) {
+        return next({ status: 404, msg: "Ya dun fucked up"});
+    }
+
+    selectPrepositions(capitalisedCase).then((data) => {
         return res.status(200).send(data.rows)
     })
 }
 
-function getPrepositionsByCase(req, res, next) {
-    const { grammaticalCase } = req.params;
-    const capitalisedCase = capitaliseFirst(grammaticalCase);
-    const acceptedValues = ["Genitive", "Instrumental", "Locative", "Accusative"];
 
-    if (!acceptedValues.includes(capitalisedCase)) {
-        return next({ status: 404, msg: "Ya dun fucked up" });
-    }
-
-    selectPrepositionsByCase(capitalisedCase)
-        .then((data) => {
-            res.status(200).send(data.rows);
-        })
-        .catch(next);
+function getPrepositionsByID(req, res, next) {
+    return "hello"
 }
 
-module.exports = { getPrepositions, getPrepositionsByCase }
+module.exports = { getPrepositions, getPrepositionsByID }
--- Models/prepositionModels.js
+++ Models/prepositionModels.js
@@ -1,11 +1,11 @@
 const db = require("../database/connection");
 
-function selectPrepositions() {
-    return db.query(`SELECT * FROM prepositions;`)
+function selectPrepositions(grammaticalCase) {
+    if (grammaticalCase) {
+        return db.query(`SELECT * FROM prepositions WHERE grammatical_case = $1;`, [grammaticalCase]);
+    } else {
+        return db.query(`SELECT * FROM prepositions;`);
+    }
 }
 
-function selectPrepositionsByCase(grammaticalCase) {
-    return db.query(`SELECT * FROM prepositions WHERE grammatical_case = $1;`, [grammaticalCase])
-}
-
-module.exports = { selectPrepositions, selectPrepositionsByCase }
+module.exports = { selectPrepositions }

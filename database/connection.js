5bd21b6b1a6d39c60b9d950e73897a861b2833f9
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2025-05-30 12:08:10 +0100

    Getting ready for hosting

--- database/connection.js
+++ database/connection.js
@@ -1,13 +1,23 @@
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || 'development';
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../ini.env") }); //Not 100% sure why this works/is needed, but without path.resolve it fails to locate the correct file
 
-const { Pool } = require("pg");
 
 if (!process.env.PGDATABASE) {
         throw new Error("No PGDatabase configured")
-    }
+}
+    
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
     
 const db = new Pool();
     
     // console.log(process.env.PGDATABASE)
 module.exports = db;
+
+console.log(process.env)
